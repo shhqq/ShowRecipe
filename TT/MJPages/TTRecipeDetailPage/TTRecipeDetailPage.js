953e4ee177a65f4ee323ae3030ef399ea86f0a94
shhqq <shhicc@163.com>
2019-09-03 15:43:12 +0800

    20190903 Initialize the repository.

---
+++ TT/MJPages/TTRecipeDetailPage/TTRecipeDetailPage.js
@@ -0,0 +1,74 @@
+// MJPages/TTRecipeDetailPage/TTRecipeDetailPage.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+    pictures: [
+      'http://pic.9ht.com/up/2018-8/2018082016513659505.jpg',
+      'http://img1.imgtn.bdimg.com/it/u=3370442833,4268965594&fm=26&gp=0.jpg',
+      'http://www.touxiang.cn/uploads/20130204/04-081928_459.jpg',
+      'http://img1.imgtn.bdimg.com/it/u=3370442833,4268965594&fm=26&gp=0.jpg',
+      'http://pic.9ht.com/up/2018-8/2018082016513659505.jpg',
+    ],
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
